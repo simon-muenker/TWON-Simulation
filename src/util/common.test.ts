b07d6cb484da4818966a4acf9b3ae6df36c2dbc9
Simon Münker <muenker@uni-trier.de>
2024-12-22 23:50:35 +0100

    run lin and format

--- src/util/common.test.ts
+++ src/util/common.test.ts
@@ -6,10 +6,10 @@ const testCases = [
   { k: 0.0, b: 0.0, expected: 0.0 },
   { k: 0.5, b: 0.5, expected: 0.8 },
   { k: 1.0, b: 1.0, expected: 1.0 },
-]
+];
 
 for (const { k, b, expected } of testCases) {
   Deno.test(`activation(k=${k}, b=${b})`, () => {
     expect(activation(k, b)).toBeCloseTo(expected, 2);
   });
-} 
+}
